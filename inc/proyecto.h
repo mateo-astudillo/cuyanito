00e3c0af01880b867436ae1c8f3f26848a169c3e
Mateo Astudillo <ast.mateo@gmail.com>
2022-10-06 11:52:03 -0300

    add proyecto.h

---
+++ inc/proyecto.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define EMPLOYED_PATH "/home/mateo/Projects/C/Proyecto/data/empleados.dat"
+#define BUSINESS_PATH "/home/mateo/Projects/C/Proyecto/data/empleados.dat"
--- src/main.c
+++ src/main.c
@@ -4,4 +4,5 @@
 int main(int argc, char **argv){
   menu();
   return 0;
+
 }
